c32ee3e5f2e2a63bb99d4371a547e7a317106d0d
chuxiaolai <2511513270@qq.com>
2024-05-09 20:50:56 +0800

    MedAI-backend-master

---
+++ MedAI-backend-master/src/main/java/com/shier/shierbi/bizmq/BiMqConstant.java
@@ -0,0 +1,11 @@
+package com.shier.shierbi.bizmq;
+
+/**
+ * 给交换机一个固定的值
+ */
+public interface BiMqConstant {
+    String BI_EXCHANGE_NAME = "bi_xhl_exchange";
+    String BI_QUEUE_NAME = "bi_xhl_queue";
+
+    String BI_ROUTING_KEY = "bi_xhl_routingKey";
+}
---
+++ MedAI-backend-master/src/main/java/com/shier/shierbi/constant/FileConstant.java
@@ -0,0 +1,14 @@
+package com.shier.shierbi.constant;
+
+/**
+ * 文件常量
+ *
+ * @author Shier
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     */
+    String COS_HOST = "https://blog.csdn.net/qq_56098191";
+}
