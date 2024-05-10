fd8239b56c8351f10d66b073fd3fb9dd153cc0a6
chuxiaolai <2511513270@qq.com>
2024-05-10 09:54:23 +0800

    MedAI-frontend-master

--- MedAI-frontend-master/src/access.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://umijs.org/zh-CN/plugins/plugin-access
- * */
-export default function access(initialState: { currentUser?: API.LoginUserVO } | undefined) {
-  const { currentUser } = initialState ?? {};
-  return {
-    canAdmin: currentUser && currentUser.userRole === 'admin',
-  };
-}
